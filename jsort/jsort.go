7aee639225b613cd13fa60f45bbe328e477c4cbd
Analyse4 <analysek4@gmail.com>
2019-03-22 00:06:00 +0800

    write select sort write test associate select sort

--- jsort/jsort.go
+++ jsort/jsort.go
@@ -1,15 +1,34 @@
 package jsort
 
+import "fmt"
+
 type Comparable interface {
 	//Less(Comparable, Comparable) int
-	Less(i interface{}, j interface{}) int
-	Exch([]interface{}, int, int)
+	Less(i interface{}) int
+	//Exch([]interface{}, int, int)
 }
 
-func SelectS(a []Comparable) {
-
+func SelectSort(a []Comparable) {
+	for i := 0; i < len(a); i++ {
+		for j := i + 1; j < len(a); j++ {
+			if a[i].Less(a[j]) > 0 {
+				min := a[j]
+				a[j] = a[i]
+				a[i] = min
+			}
+		}
+	}
 }
 
-func IsSort(a []Comparable) int {
+func IsSort(a []Comparable) bool {
+	for i, v := range a[:len(a)-2] {
+		if v.Less(a[i+1]) > 0 {
+			return false
+		}
+	}
+	return true
+}
 
+func Show(a []Comparable) {
+	fmt.Println(a)
 }
--- main.go
+++ main.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"algorithm/jsort"
+)
+
+type word int
+
+func (w word) Less(i interface{}) int {
+	if int(w)-int(i.(word)) < 0 {
+		return -1
+	} else if int(w)-int(i.(word)) > 0 {
+		return 1
+	} else {
+		return 0
+	}
+}
+
+func main() {
+	arr := []word{word(8), word(7), word(4), word(2)}
+	arrCov := make([]jsort.Comparable, len(arr))
+	for i, v := range arr {
+		arrCov[i] = jsort.Comparable(v)
+	}
+
+	jsort.SelectSort(arrCov)
+
+	jsort.Show(arrCov)
+}
